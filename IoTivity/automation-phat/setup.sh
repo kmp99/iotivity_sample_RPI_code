9697f1c631fd1346b56ed6cb7b7c50e0f5f39d21
DESKTOP-QVMHNLF\Xtrans <krishan@xtranssolutions.com>
2019-10-15 19:29:48 +0530

    Setup.sh Update with new folder Name

--- IoTivity/automation-phat/setup.sh
+++ IoTivity/automation-phat/setup.sh
@@ -2,10 +2,10 @@
 CURPWD=`pwd`
 PROJNAME=${PWD##*/}
 
-cp ${EXAMPLEPATH}/Iotivity-Raspberry-Pi-Code/IoTivity/automation-phat/SConscript ${CURPWD}/
-cp ${EXAMPLEPATH}/Iotivity-Raspberry-Pi-Code/IoTivity/automation-phat/automation-phat-config.json ${CURPWD}/${PROJNAME}-config.json
-cp ${EXAMPLEPATH}/Iotivity-Raspberry-Pi-Code/pi-boards/automation-phat/automation-phat.json ${CURPWD}/${PROJNAME}.json
-cp ${EXAMPLEPATH}/Iotivity-Raspberry-Pi-Code/IoTivity/automation-phat/automation-phat.cpp ${CURPWD}/src/${PROJNAME}.cpp
-cp ${EXAMPLEPATH}/Iotivity-Raspberry-Pi-Code/IoTivity/automation-phat/automation-phat-gen.cpp ${CURPWD}/src/${PROJNAME}-gen.cpp
-cp ${EXAMPLEPATH}/Iotivity-Raspberry-Pi-Code/pi-boards/automation-phat/automation-hat.py ${CURPWD}/bin/
-cp ${EXAMPLEPATH}/Iotivity-Raspberry-Pi-Code/pi-boards/automation-phat/lcdlib.py ${CURPWD}/bin/
+cp ${EXAMPLEPATH}/iotivity_sample_RPI_code/IoTivity/automation-phat/SConscript ${CURPWD}/
+cp ${EXAMPLEPATH}/iotivity_sample_RPI_code/IoTivity/automation-phat/automation-phat-config.json ${CURPWD}/${PROJNAME}-config.json
+cp ${EXAMPLEPATH}/iotivity_sample_RPI_code/pi-boards/automation-phat/automation-phat.json ${CURPWD}/${PROJNAME}.json
+cp ${EXAMPLEPATH}/iotivity_sample_RPI_code/IoTivity/automation-phat/automation-phat.cpp ${CURPWD}/src/${PROJNAME}.cpp
+cp ${EXAMPLEPATH}/iotivity_sample_RPI_code/IoTivity/automation-phat/automation-phat-gen.cpp ${CURPWD}/src/${PROJNAME}-gen.cpp
+cp ${EXAMPLEPATH}/iotivity_sample_RPI_code/pi-boards/automation-phat/automation-hat.py ${CURPWD}/bin/
+cp ${EXAMPLEPATH}/iotivity_sample_RPI_code/pi-boards/automation-phat/lcdlib.py ${CURPWD}/bin/
